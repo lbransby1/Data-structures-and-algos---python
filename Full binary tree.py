19d7f17cbf8ed5ea65ba6e600dd56269252e829e
lbransby1 <luke.bransby15@gmail.com>
2021-09-11 08:37:20 +0100

    Enter

---
+++ Full binary tree.py
@@ -0,0 +1,15 @@
+#binary tree theorem
+
+# Let, i = the number of internal nodes
+#        n = be the total number of nodes
+#        l = number of leaves
+#       λ = number of levels
+# The number of leaves is i + 1.
+# The total number of nodes is 2i + 1.
+# The number of internal nodes is (n – 1) / 2.
+# The number of leaves is (n + 1) / 2.
+# The total number of nodes is 2l – 1.
+# The number of internal nodes is l – 1.
+# The number of leaves is at most 2λ - 1.
+
+
